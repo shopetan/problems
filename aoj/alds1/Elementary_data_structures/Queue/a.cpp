cd9e7afd04614d861c3cd1fe612f51bd45ff0ac4
Shohei.K <shoppe_shopetan@yahoo.co.jp>
2017-06-19 23:51:29 +0900

    合ってそうなのにWA

--- aoj/alds1/Elementary_data_structures/Queue/a.cpp
+++ aoj/alds1/Elementary_data_structures/Queue/a.cpp
@@ -1,50 +1,54 @@
 # include <bits/stdc++.h>
 
-# define FOR(i,a,b) for (int i=(a);i<(b);i++)
-# define RFOR(i,a,b) for (int i=(b)-1;i>=(a);i--)
 # define REP(i, n) for (int i = 0; i < (int)(n); i++)
-# define RREP(i,n) for (int i=(n)-1;i>=0;i--)
-
-# define int(x) int x; scanf("%d",&x);
-# define vint(v,n) vector<int> v(n); REP(i,n) scanf("%d", &v[i]);
-# define string(x) string x; cin >> x;
 
 using namespace std;
 
-namespace utils{
-  template <typename T> void print(vector<vector<T>> mat) {
-    REP (i, mat.size()) {
-      REP (j, mat[0].size()) cout << mat[i][j] << ' ';
-      cout << endl;
-    }
+class Process {
+  string name;
+  int time;
+public:
+  Process(int t, char *n){
+    time = t;
+    name = n;
   }
-
-  template <typename T> void print(vector<T> v) {
-    REP (i, v.size())
-      if (i >= v.size()-1)
-	cout << v[i];
-      else
-	cout << v[i] << ' ';
-    
-    cout << endl;
+  int take(int n) {
+    time -= n;
+    if(time > 0)
+      return -1;
+    else
+      return time + n;
   }
-
-  template <typename T> pair<T, T> shape(vector<vector<T>> mat) {
-    int d1, d2;
-
-    d1 = mat.size();
-    if (d1 > 0) d2 = mat[0].size();
-    else int d2 = 0;
-    cout << "(" << d1 << ", " << d2 << ")" << endl;
-    return make_pair(0, 0);
+  string getName(){
+    return name;
   }
-
-  template <typename T> vector<vector<T>> empty(int n, int m) {
-    vector<vector<T>> mat(n, vector<T>(m));
-    return mat;
-  }
-}
+};
 
 
 int main() {
+  queue<Process> que;
+  int i_process,i_quantum;
+  int i_now_time;
+
+  scanf("%d %d",&i_process,&i_quantum);
+  
+  REP(i,i_process){
+    char str[11];
+    int time;
+    scanf("%s %d", str, &time);
+    que.push(Process(time,str));
+  }
+  while(!que.empty()){
+    int taken = que.front().take(i_quantum);
+    if(taken == -1){
+      i_now_time += i_quantum;
+      que.push(que.front());
+    }
+    else{
+      i_now_time += taken;
+      printf("%s %d\n",que.front().getName().c_str(), i_now_time);
+    }
+    que.pop();
+  }
+  
 }
