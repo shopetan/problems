2986af2b827850189c5ed96f55c060e083feabe2
Shohei.K <shoppe_shopetan@yahoo.co.jp>
2017-06-22 21:09:45 +0900

    ADD ADSL1_4_A-B

---
+++ aoj/alds1/Search/Binary_Search/a.cpp
@@ -0,0 +1,77 @@
+# include <bits/stdc++.h>
+
+# define FOR(i,a,b) for (int i=(a);i<(b);i++)
+# define RFOR(i,a,b) for (int i=(b)-1;i>=(a);i--)
+# define REP(i, n) for (int i = 0; i < (int)(n); i++)
+# define RREP(i,n) for (int i=(n)-1;i>=0;i--)
+
+# define int(x) int x; scanf("%d",&x);
+# define vint(v,n) vector<int> v(n); REP(i,n) scanf("%d", &v[i]);
+# define string(x) string x; cin >> x;
+
+using namespace std;
+
+namespace utils{
+  template <typename T> void print(vector<vector<T>> mat) {
+    REP (i, mat.size()) {
+      REP (j, mat[0].size()) cout << mat[i][j] << ' ';
+      cout << endl;
+    }
+  }
+
+  template <typename T> void print(vector<T> v) {
+    REP (i, v.size())
+      if (i >= v.size()-1)
+	cout << v[i];
+      else
+	cout << v[i] << ' ';
+    
+    cout << endl;
+  }
+
+  template <typename T> pair<T, T> shape(vector<vector<T>> mat) {
+    int d1, d2;
+
+    d1 = mat.size();
+    if (d1 > 0) d2 = mat[0].size();
+    else int d2 = 0;
+    cout << "(" << d1 << ", " << d2 << ")" << endl;
+    return make_pair(0, 0);
+  }
+
+  template <typename T> vector<vector<T>> empty(int n, int m) {
+    vector<vector<T>> mat(n, vector<T>(m));
+    return mat;
+  }
+}
+
+
+int main() {
+  int(n);
+  vint(A,n);
+  int(m);
+  vint(B,m);
+
+  int cnt = 0;
+  int mid;
+
+  REP(i,m){
+    int left = 0;
+    int right = n;
+
+    while(left < right){
+      mid = (left + right)/2;
+      if(B[i] == A[mid]){
+	cnt++;
+	break;
+      }
+      else if(B[i] < A[mid]){
+	right = mid;
+      }
+      else{
+	left = mid + 1;
+      }
+    }
+  }
+  cout << cnt << endl;
+}
