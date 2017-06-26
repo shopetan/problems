78733910120825680e55eeaca984cf72756d9149
Shohei.K <shoppe_shopetan@yahoo.co.jp>
2017-06-27 00:04:57 +0900

    ADD problem

---
+++ aoj/alds1/Search/Dictionary/a.cpp
@@ -0,0 +1,69 @@
+# include <bits/stdc++.h>
+
+# define FOR(i,a,b) for (int i=(a);i<(b);i++)
+# define RFOR(i,a,b) for (int i=(b)-1;i>=(a);i--)
+# define REP(i, n) for (int i = 0; i < (int)(n); i++)
+# define RREP(i,n) for (int i=(n)-1;i>=0;i--)
+
+# define int(x) int x; scanf("%d",&x);
+# define vint(v,n) vector<int> v(n); REP(i,n) scanf("%d", &v[i]);
+# define string(x) string x; cin >> x;
+
+using namespace std;
+
+namespace utils{
+  template <typename T> void print(vector<vector<T>> mat) {
+    REP (i, mat.size()) {
+      REP (j, mat[0].size()) cout << mat[i][j] << ' ';
+      cout << endl;
+    }
+  }
+
+  template <typename T> void print(vector<T> v) {
+    REP (i, v.size())
+      if (i >= v.size()-1)
+	cout << v[i];
+      else
+	cout << v[i] << ' ';
+    
+    cout << endl;
+  }
+
+  template <typename T> pair<T, T> shape(vector<vector<T>> mat) {
+    int d1, d2;
+
+    d1 = mat.size();
+    if (d1 > 0) d2 = mat[0].size();
+    else int d2 = 0;
+    cout << "(" << d1 << ", " << d2 << ")" << endl;
+    return make_pair(0, 0);
+  }
+
+  template <typename T> vector<vector<T>> empty(int n, int m) {
+    vector<vector<T>> mat(n, vector<T>(m));
+    return mat;
+  }
+}
+
+
+int main() {
+  unordered_set<string> dict;
+  int(n);
+
+  REP(i,n){
+    char word[13] = {};
+    char cmd[7] = {};
+    scanf("%s %s",cmd,word);
+    if(cmd[0] == 'i')
+      dict.insert(word);
+    else{
+      unordered_set<string>::const_iterator got = dict.find(word);
+      if(got == dict.end())
+	cout << "no" << endl;
+      else
+	cout << "yes" << endl;
+    }
+
+  }
+  
+}
