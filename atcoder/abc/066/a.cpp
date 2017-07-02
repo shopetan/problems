582fe467155100794b6b448751ceee04d0bb3250
Shohei.K <shoppe_shopetan@yahoo.co.jp>
2017-07-02 20:18:04 +0900

    Atcoder 066 abc

---
+++ atcoder/abc/066/a.cpp
@@ -0,0 +1,62 @@
+# include <bits/stdc++.h>
+
+# define FOR(i,a,b) for (int i=(a);i<(b);i++)
+# define RFOR(i,a,b) for (int i=(b)-1;i>=(a);i--)
+# define REP(i, n) for (int i = 0; i < (int)(n); i++)
+# define RREP(i,n) for (int i=(n)-1;i>=0;i--)
+
+# define int(x) int x; scanf("%d",&x);
+# define vint(v,n) vector<int> v(n); REP(i,n) scanf("%d", &v[i]);
+# define string(x) string x; cin >> x;
+
+using namespace std;
+
+namespace utils{
+  template <typename T> void print(vector<vector<T>> mat) {
+    REP (i, mat.size()) {
+      REP (j, mat[0].size()) cout << mat[i][j] << ' ';
+      cout << endl;
+    }
+  }
+
+  template <typename T> void print(vector<T> v) {
+    REP (i, v.size())
+      if (i >= v.size()-1)
+	cout << v[i];
+      else
+	cout << v[i] << ' ';
+    
+    cout << endl;
+  }
+
+  template <typename T> pair<T, T> shape(vector<vector<T>> mat) {
+    int d1, d2;
+
+    d1 = mat.size();
+    if (d1 > 0) d2 = mat[0].size();
+    else int d2 = 0;
+    cout << "(" << d1 << ", " << d2 << ")" << endl;
+    return make_pair(0, 0);
+  }
+
+  template <typename T> vector<vector<T>> empty(int n, int m) {
+    vector<vector<T>> mat(n, vector<T>(m));
+    return mat;
+  }
+}
+
+
+int main() {
+  vint(A,3);
+  int min = 100000;
+  
+  for(int i = 0;i < 3;i++){
+    for(int j = i;j < 3;j++){
+      if(i != j && ((A[i] + A[j]) < min)){
+	min = (A[i] + A[j]);
+      }
+    }
+  }
+  cout << min << endl;
+  
+}
---
+++ atcoder/abc/066/c.cpp
@@ -0,0 +1,74 @@
+# include <bits/stdc++.h>
+
+# define FOR(i,a,b) for (int i=(a);i<(b);i++)
+# define RFOR(i,a,b) for (int i=(b)-1;i>=(a);i--)
+# define REP(i, n) for (int i = 0; i < (int)(n); i++)
+# define RREP(i,n) for (int i=(n)-1;i>=0;i--)
+
+# define int(x) int x; scanf("%d",&x);
+# define vint(v,n) vector<int> v(n); REP(i,n) scanf("%d", &v[i]);
+# define string(x) string x; cin >> x;
+
+using namespace std;
+
+namespace utils{
+  template <typename T> void print(vector<vector<T>> mat) {
+    REP (i, mat.size()) {
+      REP (j, mat[0].size()) cout << mat[i][j] << ' ';
+      cout << endl;
+    }
+  }
+
+  template <typename T> void print(vector<T> v) {
+    REP (i, v.size())
+      if (i >= v.size()-1)
+	cout << v[i];
+      else
+	cout << v[i] << ' ';
+    
+    cout << endl;
+  }
+
+  template <typename T> pair<T, T> shape(vector<vector<T>> mat) {
+    int d1, d2;
+
+    d1 = mat.size();
+    if (d1 > 0) d2 = mat[0].size();
+    else int d2 = 0;
+    cout << "(" << d1 << ", " << d2 << ")" << endl;
+    return make_pair(0, 0);
+  }
+
+  template <typename T> vector<vector<T>> empty(int n, int m) {
+    vector<vector<T>> mat(n, vector<T>(m));
+    return mat;
+  }
+}
+
+
+int main() {
+  int(n);
+  long long A[n];
+  
+  REP(i,n){
+    int x;
+    cin >> x;
+
+    A[i] = x;
+  }
+
+  for(int i = (n-1);i >= 0;i-=2)
+    cout << A[i] << " ";
+  if(n%2 == 0){
+    for(int i = 0;i <= n-2;i+=2)
+      cout << A[i] << " ";
+    cout<< endl;
+  }else{
+    for(int i = 1;(i < n-2) && (n-2 >= 0);i+=2)
+      cout << A[i] << " ";
+    if(n-2 >= 0)
+      cout << A[n-2] << endl;
+    else
+      cout << endl;
+  }
+}
