7b4612d37d0f3b5c8ded45bff63bfdf2e63da8fb
Shohei.K <shoppe_shopetan@yahoo.co.jp>
2017-07-12 01:18:32 +0900

    ADD 32

---
+++ yukicoder/rank1.0/32.cpp
@@ -0,0 +1,71 @@
+# include <bits/stdc++.h>
+
+# define FOR(i,a,b) for (int i=(a);i<(b);i++)
+# define RFOR(i,a,b) for (int i=(b)-1;i>=(a);i--)
+# define REP(i, n) for (int i = 0; i < (int)(n); i++)
+# define RREP(i,n) for (int i=(n)-1;i>=0;i--)
+
+# define int(x) int x; scanf("%d",&x);
+# define vint(v,n) vector<int> v(n); REP(i,n) scanf("%d", &v[i]);
+# define string(x) string x; cin >> x;
+
+using namespace std;
+
+namespace utils{
+  template <typename T> void print(vector<vector<T>> mat) {
+    REP (i, mat.size()) {
+      REP (j, mat[0].size()) cout << mat[i][j] << ' ';
+      cout << endl;
+    }
+  }
+
+  template <typename T> void print(vector<T> v) {
+    REP (i, v.size())
+      if (i >= v.size()-1)
+	cout << v[i];
+      else
+	cout << v[i] << ' ';
+    
+    cout << endl;
+  }
+
+  template <typename T> pair<T, T> shape(vector<vector<T>> mat) {
+    int d1, d2;
+
+    d1 = mat.size();
+    if (d1 > 0) d2 = mat[0].size();
+    else int d2 = 0;
+    cout << "(" << d1 << ", " << d2 << ")" << endl;
+    return make_pair(0, 0);
+  }
+
+  template <typename T> vector<vector<T>> empty(int n, int m) {
+    vector<vector<T>> mat(n, vector<T>(m));
+    return mat;
+  }
+}
+
+
+int main() {
+  int(l);
+  int(m);
+  int(n);
+
+  int l_rem;
+  int m_rem;
+  int n_rem;
+  int cnt = 0;
+
+  n_rem = n%25;
+  n = n/25;
+
+  m += n;
+  m_rem = m%4;
+  m = m/4;
+
+  l += m;
+  l_rem = l%10;
+  l = l/10;
+
+  cout << l_rem +  m_rem + n_rem << endl;  
+}
