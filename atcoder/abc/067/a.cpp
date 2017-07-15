e106b064fc85d1c019cf941afa7e63540d6c8d10
Shohei.K <shoppe_shopetan@yahoo.co.jp>
2017-07-16 00:25:21 +0900

    ADD atcoder 067 a

--- atcoder/abc/067/a.cpp
+++ atcoder/abc/067/a.cpp
@@ -47,4 +47,11 @@ namespace utils{
 
 
 int main() {
+  int a,b;
+  cin >> a >> b;
+
+  if(a%3 ==0) cout << "Possible" << endl;
+  else if(b%3 ==0) cout << "Possible" << endl;
+  else if((a+b)%3 ==0) cout << "Possible" << endl;
+  else cout << "Impossible" << endl;
 }
