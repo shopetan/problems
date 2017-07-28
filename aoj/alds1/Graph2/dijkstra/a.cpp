f3829b58e2b9d0536ef00c5045157b9ad33161e3
Shohei.K <shoppe_shopetan@yahoo.co.jp>
2017-07-28 20:31:48 +0900

    ADD dijkstra

---
+++ aoj/alds1/Graph2/dijkstra/a.cpp
@@ -0,0 +1,86 @@
+# include <bits/stdc++.h>
+
+# define FOR(i,a,b) for (int i=(a);i<(b);i++)
+# define RFOR(i,a,b) for (int i=(b)-1;i>=(a);i--)
+# define REP(i, n) for (int i = 0; i < (int)(n); i++)
+# define RREP(i,n) for (int i=(n)-1;i>=0;i--)
+
+# define int(x) int x; scanf("%d",&x);
+# define vint(v,n) vector<int> v(n); REP(i,n) scanf("%d", &v[i]);
+# define string(x) string x; cin >> x;
+
+using namespace std;
+
+namespace utils{
+  template <typename T> void print(vector<vector<T>> mat) {
+    REP (i, mat.size()) {
+      REP (j, mat[0].size()) cout << mat[i][j] << ' ';
+      cout << endl;
+    }
+  }
+
+  template <typename T> void print(vector<T> v) {
+    REP (i, v.size())
+      if (i >= v.size()-1)
+	cout << v[i];
+      else
+	cout << v[i] << ' ';
+    
+    cout << endl;
+  }
+
+  template <typename T> pair<T, T> shape(vector<vector<T>> mat) {
+    int d1, d2;
+
+    d1 = mat.size();
+    if (d1 > 0) d2 = mat[0].size();
+    else int d2 = 0;
+    cout << "(" << d1 << ", " << d2 << ")" << endl;
+    return make_pair(0, 0);
+  }
+
+  template <typename T> vector<vector<T>> empty(int n, int m) {
+    vector<vector<T>> mat(n, vector<T>(m));
+    return mat;
+  }
+}
+
+#define INF 1<<30
+
+int main() {
+  int(n);
+  vector<vector<pair<int, int>>> g(n);
+  vector<int> dp(n);
+  queue<int> q;
+
+  REP(i,n){
+    int u,k;
+    cin >> u >> k;
+    REP(j,k){
+      int c,v;
+      cin >> c >> v;
+      g[u].push_back(make_pair(c,v));
+    }
+  }
+  fill(dp.begin(),dp.end(), INF);
+  dp[0] = 0;
+  q.push(0);
+
+  while(!q.empty()){
+    int k =  q.front();
+    q.pop();
+
+    REP(i,g[k].size()){
+      int next = g[k][i].first;
+      int w = g[k][i].second;
+      if(dp[k] + w < dp[next]){
+	dp[next] = dp[k] + w;
+	q.push(next);
+	cout << k << " " << next << " " << dp[next] << endl;
+      }
+    }
+  }
+  REP(i,n)
+    cout << i << " " << dp[i] << endl;
+  
+}
